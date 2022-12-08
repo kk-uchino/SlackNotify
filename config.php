1e604acaf04243b32af18a61d0616a834a4cf0c0
uchino <44802818+kk-uchino@users.noreply.github.com>
2022-12-08 23:15:29 +0900

    プラグインの説明を追加

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 $title = 'Slack通知プラグイン';
-$description = '';
-$author = '';
+$description = 'メールフォームの送信をSlackに通知できるプラグインです。';
+$author = 'Koki Uchino';
 $url = '';
 $adminLink = ['plugin' => 'slack_notify', 'controller' => 'slack_notify', 'action' => 'config'];
